544ac55ee916726319916d472e0dfe7a9a43f419
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-02-21 17:09:29 +0000

    Add files via upload

---
+++ DepositCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int depositLenght = int.Parse(Console.ReadLine());
+            double annualInterest = double.Parse(Console.ReadLine());
+
+            
+            double currentInterest = (deposit * annualInterest) / 100;
+            
+            double monthlyInterest = currentInterest / 12;
+            
+            double finalsum = deposit + (depositLenght * monthlyInterest);
+            Console.WriteLine(finalsum);
+        }
+    }
+}
