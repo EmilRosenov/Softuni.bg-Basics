f166dd04bbc3f0c783686471687d810897581972
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-06 00:13:32 +0000

    Add files via upload

---
+++ ConditionalStatementsExercise/Time+15Minutes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Time_15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            // console.writeline($"{hour}:{mins}
+            int minutes = int.Parse(Console.ReadLine());
+            minutes = minutes + 15;
+            //minutes += 15;
+
+            if (minutes > 59)
+            {
+                hours = hours + 1;
+                minutes = minutes - 60;
+            }
+
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            if (minutes < 10)
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+
+            else
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+        }
+    }
+}
