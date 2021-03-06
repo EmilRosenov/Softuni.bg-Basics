2753a6449fc2b5055af244ff45aa070e0a6276da
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-06 00:15:40 +0000

    Add files via upload

---
+++ ConditionalStatementsExercise/Scholarship.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Scholarship
+{
+    class Scholarship
+    {
+        static void Main(string[] args)
+        {
+            double minIcome = double.Parse(Console.ReadLine());
+
+            double averageGrade = double.Parse(Console.ReadLine());
+            double minWage = double.Parse(Console.ReadLine());
+
+            double SocialScholarship = Math.Floor(minWage * 0.35);
+            double exellentScholarship = Math.Floor(averageGrade * 25);
+
+            if (minIcome > minWage && averageGrade < 4.50)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+            else if (minWage > minIcome && averageGrade < 4.50)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+
+            else if (minWage < minIcome && averageGrade < 5.50)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+
+            else if (minWage > minIcome && averageGrade < 5.50)
+            {
+                Console.WriteLine($"You get a Social scholarship {SocialScholarship} BGN");
+            }
+
+
+            else if (minWage > minIcome && averageGrade >= 5.50)
+            {
+                Console.WriteLine($"You get a scholarship for excellent results { exellentScholarship} BGN");
+            }
+
+            else if (minWage < minIcome && averageGrade >= 5.50)
+            {
+                Console.WriteLine($"You get a scholarship for excellent results { exellentScholarship} BGN");
+            }
+
+            else if (SocialScholarship > exellentScholarship)
+            {
+                Console.WriteLine($"You get a Social scholarship{SocialScholarship} BGN");
+            }
+
+            else
+            {
+                Console.WriteLine($"You get a scholarship for excellent results { exellentScholarship} BGN");
+            }
+        }
+    }
+}
