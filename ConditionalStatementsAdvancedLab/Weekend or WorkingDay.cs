f58e7f75d74e80ef8626c026ace521c57a51e131
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-07 19:51:26 +0000

    Add files via upload

---
+++ ConditionalStatementsAdvancedLab/Weekend or WorkingDay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekendOrWorking_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
