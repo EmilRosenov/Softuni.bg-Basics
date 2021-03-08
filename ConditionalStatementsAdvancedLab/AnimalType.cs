a9ef580986defa9693c102e31b3f58c994a5ca20
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-08 14:01:05 +0000

    Add files via upload

---
+++ ConditionalStatementsAdvancedLab/AnimalType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnimalType
+{
+    class AnimalType
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "tortoise":
+                case "snake":
+                case "crocodile":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+
+            }
+        }
+    }
+}
