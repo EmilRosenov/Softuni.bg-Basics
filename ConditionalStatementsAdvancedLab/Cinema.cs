3087d3c6e8edf1c269ce0d3e96eb0da6de35a9f9
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-09 21:46:42 +0000

    Add files via upload

---
+++ ConditionalStatementsAdvancedLab/Cinema.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Cinema
+    {
+        static void Main(string[] args)
+        {
+            string projectionType = Console.ReadLine();
+            int numberOfRows = int.Parse(Console.ReadLine());
+            int numberOfColumns = int.Parse(Console.ReadLine());
+            double totalIncome = 0;
+
+
+            if (projectionType == "Premiere")
+            {
+                double income = numberOfRows * numberOfColumns;
+                totalIncome = income * 12.00;
+            }
+            else if (projectionType == "Normal")
+            {
+                double income = numberOfRows * numberOfColumns;
+                totalIncome = income * 7.50;
+            }
+            else if (projectionType == "Discount")
+            {
+                double income = numberOfRows * numberOfColumns;
+                totalIncome = income * 5.00;
+            }
+            Console.WriteLine($"{totalIncome:f2}");
+
+        }
+    }
+}
