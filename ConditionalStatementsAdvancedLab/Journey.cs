c8d5763344cd27f962cd3c3ae4037901188c0e9a
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-12 21:38:37 +0000

    Add files via upload

---
+++ ConditionalStatementsAdvancedLab/Journey.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Journey
+{
+    class Journey
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double moneySpent = 0.00;
+            string accomodation = string.Empty;
+            string destination = string.Empty;
+
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+            }
+            else if (budget > 100 && budget <= 1000)
+            {
+                destination = "Balkans";
+            }
+            else if (budget > 1000)
+            {
+                destination = "Europe";
+            }
+
+            if (destination == "Bulgaria" && season == "summer")
+            {
+                accomodation = "Camp";
+                moneySpent = budget - (budget * 0.70);
+            }
+
+            else if (destination == "Bulgaria" || season == "Winter")
+            {
+                accomodation = "Hotel";
+                moneySpent = budget - (budget * 0.30);
+            }
+            if (destination == "Balkans" && season == "summer")
+            {
+                accomodation = "Camp";
+                moneySpent = budget - (budget * 0.60);
+            }
+
+            else if (destination == "Balkans" || season == "Winter")
+            {
+                accomodation = "Hotel";
+                moneySpent = budget - (budget * 0.20);
+            }
+            if (destination == "Europe")
+            {
+                accomodation = "Hotel";
+                moneySpent = budget * 0.90;
+            }
+
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{accomodation} - {moneySpent:f2}");
+        }
+    }
+}
