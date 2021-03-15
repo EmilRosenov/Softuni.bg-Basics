4775a38f2af749d386c1ecffc5f1d858b7d153b2
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-15 15:15:36 +0000

    Add files via upload

---
+++ ConditionalStatementsAdvancedLab/OnTimeForTheExam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnTimForTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
