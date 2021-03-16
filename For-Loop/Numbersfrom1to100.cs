55df1c8c6a5e7b47607ca2d661b171ec54e32de4
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-16 04:20:21 +0000

    Add files via upload

---
+++ For-Loop/Numbersfrom1to100.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace For_Loop_Lab
+{
+    class Numbersfrom1to100
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i += 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
