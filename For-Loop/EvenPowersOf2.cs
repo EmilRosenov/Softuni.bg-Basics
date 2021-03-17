8ea0911b2f99d626b6eb160b86336c692e38b732
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-17 04:00:01 +0000

    Add files via upload

---
+++ For-Loop/EvenPowersOf2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EvenPowersOf2
+{
+    class EvenPowersOf2
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int number = 2;
+
+            for (int i = 0; i <= n; i += 2)
+            {
+                Console.WriteLine(Math.Pow(number, i));
+            }
+        }
+    }
+}
