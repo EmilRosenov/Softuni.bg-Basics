bb2f0d5a967defa6dc303d7760876547f25b9b45
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-17 04:01:58 +0000

    Add files via upload

---
+++ For-Loop/CharacterSequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CharacterSequence
+{
+    class CharacterSequence
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "softuni")
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.WriteLine(input[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.WriteLine(input[i]);
+                }
+            }
+        }
+    }
+}
