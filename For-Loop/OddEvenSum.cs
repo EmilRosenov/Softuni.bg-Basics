c0f6808a2950f63398434f780f6664da716492c4
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-18 12:20:30 +0000

    Add files via upload

---
+++ For-Loop/OddEvenSum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class OddEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double oddSum = 0;
+            double evenSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum += number;
+                }
+                else
+
+                {
+                    oddSum += number;
+                }
+            }
+
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {oddSum}");
+            }
+            else
+            {
+                double diff = Math.Abs(oddSum - evenSum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
