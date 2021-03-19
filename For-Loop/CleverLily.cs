55a5132d9d3eac1b86b7eac2fb3d2333e0bb8a0d
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-19 03:43:48 +0000

    Add files via upload

---
+++ For-Loop/CleverLily.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CleverLily
+{
+    class CleverLily
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double machinePrice = double.Parse(Console.ReadLine());
+            double toysPrice = double.Parse(Console.ReadLine());
+
+            double savedMoney = 0;
+            double toysCounter = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    savedMoney += i * 5;
+                    savedMoney--;
+                }
+                else
+                {
+                    toysCounter++;
+                }
+
+
+            }
+            savedMoney += toysCounter * toysPrice;
+            if (savedMoney >= machinePrice)
+            {
+                double diff = savedMoney - machinePrice;
+                Console.WriteLine($"Yes! {diff:f2}");
+            }
+            else
+            {
+                double diff = machinePrice - savedMoney;
+                Console.WriteLine($"No! {diff:f2}");
+            }
+        }
+    }
+}
