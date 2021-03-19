732183fdc9007c5b9c61857057d831a4aa8c95c1
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-19 03:45:23 +0000

    Add files via upload

---
+++ For-Loop/LeftAndRightSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    class LeftAndRightSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double leftSum = 0;
+            double rightSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                leftSum += number;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                rightSum += number;
+            }
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                double diff = Math.Abs(leftSum - rightSum);
+                Console.WriteLine($"No, diff = {diff}");
+            }
+        }
+    }
+}
