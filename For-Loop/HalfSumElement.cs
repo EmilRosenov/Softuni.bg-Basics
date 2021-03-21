cdca6f0a77f00be6b8a79ca016562eb7b1fe1c6e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-21 19:30:22 +0000

    Add files via upload

---
+++ For-Loop/HalfSumElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class HalfSumElement
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxNumber = double.MinValue;
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+
+
+
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            double sumNomaxNr = sum - maxNumber;
+            if (maxNumber == sumNomaxNr)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+            else
+            {
+
+                double diff = Math.Abs(maxNumber - sumNomaxNr);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+
+        }
+    }
+}
