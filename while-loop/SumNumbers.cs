f0b9f7b2d8bb8e67b0013b0b80d996fd21f54517
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-24 14:02:11 +0000

    Add files via upload

---
+++ while-loop/SumNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                sum += currentNumber;
+
+                if (sum >= n)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+            }
+        }
+    }
+}
