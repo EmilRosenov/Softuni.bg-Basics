00023ef998878e4c0577cba249d56d9d9a78f666
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-24 14:01:29 +0000

    Add files via upload

---
+++ while-loop/ReadText.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace while_loop
+{
+    class ReadText
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string text = Console.ReadLine();
+
+                if (text == "Stop")
+                {
+                    break;
+                }
+                Console.WriteLine(text);
+
+            }
+        }
+    }
+}
