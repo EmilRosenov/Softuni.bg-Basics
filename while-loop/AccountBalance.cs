5295a7e7d574c03006dcd5091040c277634c25bd
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-25 14:33:48 +0000

    Add files via upload

---
+++ while-loop/AccountBalance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class AccountBalance
+    {
+        static void Main(string[] args)
+        {
+            double sumDeposit = 0;
+
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+                if (input == "NoMoreMoney")
+                {
+                    break;
+                }
+
+                double number = double.Parse(input);
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                sumDeposit += number;
+                Console.WriteLine($"Increase: {number:f2}");
+
+            }
+
+            Console.WriteLine($"Total: {sumDeposit:f2}");
+
+        }
+    }
+}
