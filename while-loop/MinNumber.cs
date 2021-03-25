49f1ab75ef4dcbbc07d1c0061de78ef3ba915115
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-25 14:32:27 +0000

    Add files via upload

---
+++ while-loop/MinNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class MinNumber
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = int.MaxValue;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Stop")
+                {
+                    Console.WriteLine(minNumber);
+                    break;
+                }
+
+                int number = int.Parse(input);
+
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+        }
+    }
+}
