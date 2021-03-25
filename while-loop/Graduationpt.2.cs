77792a37ac3b1d30b6b899b02f6d24101eccd0b0
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-25 14:57:12 +0000

    Add files via upload

---
+++ while-loop/Graduationpt.2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Graduationpt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grade = 1;
+            int failedCounter = 0;
+            double totalRating = 0;
+
+            //graduation
+            //excluded
+
+            while (grade <= 12)
+            {
+                double rating = double.Parse(Console.ReadLine());
+
+                if (rating >= 4)
+                {
+                    totalRating += rating;
+                    grade++;
+                }
+                else
+                {
+                    failedCounter++;
+
+                    if (failedCounter == 2)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {grade} grade");
+                        break;
+                    }
+                }
+
+            }
+            double average = totalRating / 12;
+            if (grade == 13)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+            }
+
+        }
+    }
+}
