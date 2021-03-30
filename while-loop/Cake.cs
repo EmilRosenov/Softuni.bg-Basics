8181da49702dcfa6cc2ae81c2f8b7f3c3d633ccb
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-03-30 15:28:32 +0100

    Add files via upload

---
+++ while-loop/Cake.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+
+            int totalPiecesOfCake = width * lenght;
+            int sumPieces = 0;
+
+
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    int difference = totalPiecesOfCake - sumPieces;
+                    Console.WriteLine($"{difference} pieces are left.");
+                    break;
+                }
+
+                int numberOfPieces = int.Parse(input);
+                sumPieces += numberOfPieces;
+
+
+                if (sumPieces > totalPiecesOfCake)
+                {
+                    int differencce = sumPieces - totalPiecesOfCake;
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(differencce)} pieces more.");
+                    break;
+                }
+
+
+            }
+
+        }
+    }
+}
