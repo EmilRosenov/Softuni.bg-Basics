ab05eef4e2b77a62eeb3a9089f2bbc9fac40d07e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-03 19:16:09 +0100

    Add files via upload

---
+++ nested-Loops/Combinations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Combinations
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int x1 = 0; x1 <= n; x1++)
+            {
+                int x2 = 0;
+                while (x2 <= n)
+                {
+                    for (int x3 = 0; x3 <= 25; x3++)
+                    {
+                        int combinations = x1 + x2 + x3;
+
+                        if (combinations == n)
+                        {
+                            counter++;
+                        }
+                    }
+                    x2++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
