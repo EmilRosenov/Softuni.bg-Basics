d141194b09e5414b0b2f4a7194ec02dd8e7ca862
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-04 09:26:44 +0100

    Add files via upload

---
+++ nested-Loops/Travelling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Travelling
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            //double minimumBudget = double.Parse(Console.ReadLine());
+
+
+            while (destination != "End")
+            {
+                if (destination == "End")
+                {
+                    break;
+                }
+
+
+                double minimumBudget = double.Parse(Console.ReadLine());
+                double sum = 0;
+                while (sum < minimumBudget)
+                {
+
+                    double savedMoney = double.Parse(Console.ReadLine());
+                    sum += savedMoney;
+
+                    if (sum >= minimumBudget)
+                    {
+                        sum = 0;
+                        Console.WriteLine($"Going to {destination}!");
+
+                        break;
+                    }
+                }
+                destination = Console.ReadLine();
+
+            }
+        }
+    }
+}
