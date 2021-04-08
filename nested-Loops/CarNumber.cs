e1383fb18cbdf1eb49847bda3b24fd6174f497c4
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-08 23:12:50 +0100

    Add files via upload

---
+++ nested-Loops/CarNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CarNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int starInterval = int.Parse(Console.ReadLine());
+            int endInterval = int.Parse(Console.ReadLine());
+
+            int sumSecondThird = 0;
+
+            for (int first = starInterval; first <= endInterval; first++)
+            {
+                for (int second = starInterval; second <= endInterval; second++)
+                {
+                    for (int third = starInterval; third <= endInterval; third++)
+                    {
+                        
+                        for (int fourth = starInterval; fourth <= endInterval; fourth++)
+                        {
+                            sumSecondThird = second + third;
+                            if (first %2 != 0 && fourth % 2 == 0 && sumSecondThird % 2 == 0 && first > fourth)
+                            {
+                                Console.Write($"{first}{second}{third}{fourth} ");
+                            }
+                            
+                            if (first % 2 == 0 && fourth % 2 != 0 && sumSecondThird % 2 == 0 && first > fourth)
+                            {
+                                Console.Write($"{first}{second}{third}{fourth} ");
+                            }
+                          
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
