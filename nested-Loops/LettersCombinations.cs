39ad73906e9d0d13ddd76c60c303928458966848
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-10 20:05:29 +0100

    Add files via upload

---
+++ nested-Loops/LettersCombinations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LettersCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letterStartInterval = char.Parse(Console.ReadLine());
+            char secondLetterEndInterval = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+
+            int combinationsCounter = 0;
+
+            for (char x1 = letterStartInterval; x1 <= secondLetterEndInterval; x1++)
+            {
+                for (char x2 = letterStartInterval; x2 <= secondLetterEndInterval; x2++)
+                {
+                        for (char x3 = letterStartInterval; x3 <= secondLetterEndInterval; x3++)
+                        {
+                            if (x1 != thirdLetter &&
+                                x2 != thirdLetter &&
+                                x3 != thirdLetter)
+
+                            {
+                                combinationsCounter++;
+                                Console.Write($"{x1}{x2}{x3} ");
+                            }
+                        }
+                }
+                
+            }
+            Console.Write(combinationsCounter);
+
+        }
+    }
+}
