569c7342faa431db6a08d86686390e50fb37ec10
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-12 23:07:38 +0100

    Add files via upload

---
+++ nested-Loops/ChallengeTheWedding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ChallengeTheWeddingParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int men = int.Parse(Console.ReadLine());
+            int women = int.Parse(Console.ReadLine());
+            int numberOfTables = int.Parse(Console.ReadLine());
+
+            
+            for (int numberMen = 1; numberMen <= men; numberMen++)
+            {
+                
+                if (numberOfTables == 0)
+                {
+                    break;
+                }
+
+
+                for (int numberWomen = 1; numberWomen <= women; numberWomen++)
+                {
+
+                    numberOfTables -= 1;
+                    Console.Write($"({numberMen} <-> {numberWomen}) ");
+                    if (numberOfTables == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
