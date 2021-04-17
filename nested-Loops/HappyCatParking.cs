9d879dc9a3bfda9560013c420a26e0e0ec6de3bd
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-17 20:46:17 +0100

    Add files via upload

---
+++ nested-Loops/HappyCatParking.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HappyCatParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysCounter = int.Parse(Console.ReadLine());
+            int hoursPerDay = int.Parse(Console.ReadLine());
+
+            int numberOfDays = 0;
+            double currentPricePH = 0;
+            double pricePH = 0;
+            double totalPrice = 0;
+
+            for (int days = 1; days <= daysCounter; days++)
+            {
+                numberOfDays++;
+                currentPricePH = 0;
+
+                for (int hours = 1; hours <= hoursPerDay; hours++)
+                {
+                   
+                    if (days % 2 == 0 &&
+                        hours % 2 != 0)
+                    {
+                        pricePH = 2.50;
+                        currentPricePH += pricePH;
+                        totalPrice += pricePH;
+                    }
+                    else if (days % 2 != 0 &&
+                        hours % 2 == 0)
+                    {
+                        pricePH = 1.25;
+                        currentPricePH += pricePH;
+                        totalPrice += pricePH;
+                    }
+                    else
+                    {
+                        pricePH = 1.00;
+                        currentPricePH += pricePH;
+                        totalPrice += pricePH;
+                    }
+                    
+                }
+                Console.WriteLine($"Day: {numberOfDays} - {currentPricePH:f2} leva");
+            }
+            Console.WriteLine($"Total: {totalPrice:f2} leva");
+        }
+    }
+}
