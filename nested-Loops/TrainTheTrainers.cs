d4552ae43c755a46e981e877e302acf1a4d75d05
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-18 23:03:57 +0100

    Add files via upload

---
+++ nested-Loops/TrainTheTrainers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryNumber = int.Parse(Console.ReadLine());
+
+            int presentationsCounter = 0;
+            double finalAssesment = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Finish")
+                {
+                    break;
+
+                }
+
+                presentationsCounter++;
+
+                double averageGrade = 0;
+
+                for (int i = 0; i < juryNumber; i++)
+                {
+                    double gradeGiven = double.Parse(Console.ReadLine());
+                    averageGrade += gradeGiven;
+
+                }
+                averageGrade /= juryNumber;
+                finalAssesment += averageGrade;
+
+                Console.WriteLine($"{input} - {averageGrade:f2}");
+                
+            }
+            finalAssesment /= presentationsCounter;
+            Console.WriteLine($"Student's final assessment is {finalAssesment:f2}");
+        }
+    }
+}
