325a94da3ae1bc862fe3a16e0c6489e26dafd33a
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-22 23:39:16 +0100

    Add files via upload

---
+++ Programming Basics Exams/PBOnlineRetakeExam-2and3May19/VetParking/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace VetParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberDays = double.Parse(Console.ReadLine());
+
+
+            double numberOfHoursPerDay = double.Parse(Console.ReadLine());
+            double totalTax = 0;
+            
+            
+
+
+            for (int day= 1; day <= numberDays; day++)
+            {
+                double taxPerDay = 0;
+                double tax = 0;
+
+                for (int hour = 1; hour <= numberOfHoursPerDay; hour++)
+                {
+                    if (day % 2 != 0 && hour % 2 == 0)
+                    {
+                        tax += 1.25;
+                    }
+                    else if (day % 2 == 0 && hour % 2 != 0)
+                    {
+                        tax += 2.50;
+                    }
+                    else
+                    {
+                        tax += 1;
+                    }
+                    
+                }
+                taxPerDay = tax;
+                totalTax += taxPerDay;
+                Console.WriteLine($"Day: {day} - {taxPerDay:f2} leva");
+            }
+
+            Console.WriteLine($"Total: {totalTax:f2} leva");
+        }
+    }
+}
