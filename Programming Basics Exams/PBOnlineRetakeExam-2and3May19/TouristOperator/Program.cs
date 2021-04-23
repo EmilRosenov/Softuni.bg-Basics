9d5f530262345fdbcf1607be8be576944dba2f33
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-23 23:28:34 +0100

    Add files via upload

---
+++ Programming Basics Exams/PBOnlineRetakeExam-2and3May19/TouristOperator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TouristOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            double numberItemsBought = 0;
+            double totalPriceItemsBought = 0;
+            double moneyNeeded = 0;
+            bool isBudgetEnough = true;
+
+
+            while (input != "Stop")
+            {
+                
+                double priceOfProduct = double.Parse(Console.ReadLine());
+
+                if (priceOfProduct > budget)
+                {
+                    moneyNeeded = priceOfProduct - budget;
+                    Console.WriteLine("You don't have enough money!");
+                    Console.WriteLine($"You need {moneyNeeded:f2} leva!");
+                    return;
+                }
+
+                numberItemsBought++;
+               
+                   
+                if (numberItemsBought % 3==0)
+                {
+                    priceOfProduct *= 0.5;
+                }
+
+                budget -= priceOfProduct;
+                totalPriceItemsBought += priceOfProduct;
+
+                input = Console.ReadLine();
+            }
+            
+             if (isBudgetEnough)
+             {
+             Console.WriteLine($"You bought {numberItemsBought} products for {totalPriceItemsBought:f2} leva.");
+             }
+           
+        }
+    }
+}
