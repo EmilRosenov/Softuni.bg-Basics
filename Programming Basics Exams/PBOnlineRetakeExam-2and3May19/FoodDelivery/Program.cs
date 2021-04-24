f127bb8438d5e7faf1e51f60b22bdabd1d6f3673
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-24 04:14:45 +0100

    Add files via upload

---
+++ Programming Basics Exams/PBOnlineRetakeExam-2and3May19/FoodDelivery/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberChickenMenus = double.Parse(Console.ReadLine());  
+            double numberFishMenus = double.Parse(Console.ReadLine());  
+            double numberVegiMenus = double.Parse(Console.ReadLine());
+
+            double chickenMenu = 10.35;
+            double fishMenu = 12.40;
+            double vegiMenu = 8.15;
+
+            double totalChicken = numberChickenMenus * chickenMenu;
+            double totalFish = numberFishMenus * fishMenu;
+            double totalVegi = numberVegiMenus * vegiMenu;
+
+            double totalPriceNodelivery = totalChicken + totalFish + totalVegi;
+            double dessert = totalPriceNodelivery * 0.2;
+            double delivery = 2.50;
+
+            double total = totalPriceNodelivery + dessert + delivery;
+
+            Console.WriteLine($"Total: {total:f2}");
+
+        }
+    }
+}
