1e78fa023323bce71fa473a4bc32ee95f318325a
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-27 13:46:42 +0100

    Add files via upload

---
+++ Programming Basics Exams/PBOnlineRetakeExam-2and3May19/DivisionWithoutRemainder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DivisionWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double dividedby2group = 0;
+            double dividedby3group = 0;
+            double dividedby4group = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    dividedby2group++;
+                }
+                if (number % 3 == 0)
+                {
+                    dividedby3group++;
+                }
+                if (number % 4 == 0)
+                {
+                    dividedby4group++;
+                }
+
+            }
+
+            double percentageDiv2 = dividedby2group / n * 100;
+            double percentageDiv3 = dividedby3group / n * 100;
+            double percentageDiv4 = dividedby4group / n * 100;
+
+            Console.WriteLine($"{percentageDiv2:f2}%");
+            Console.WriteLine($"{percentageDiv3:f2}%");
+            Console.WriteLine($"{percentageDiv4:f2}%");
+
+        }
+    }
+}
