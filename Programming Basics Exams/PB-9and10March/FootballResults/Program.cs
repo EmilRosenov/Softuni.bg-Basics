42ccf766a63c8a8d960661f8437efef5cf2f5e06
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-28 18:41:34 +0100

    Add files via upload

---
+++ Programming Basics Exams/PB-9and10March/FootballResults/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace FootballResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gamesPlayed = 3;
+
+            int homeWins = 0;
+            int awayWins = 0;
+            int draws = 0;
+            
+
+            for (int i = 0; i < gamesPlayed; i++)
+            {
+                string result = Console.ReadLine();
+
+                if (result[0] > result[2])
+                {
+                    homeWins++;
+                }
+                else if (result[0] < result[2])
+                {
+                    awayWins++;
+                }
+                else
+                {
+                    draws++;
+                }
+               
+            }
+            Console.WriteLine($"Team won {homeWins} games.");
+            Console.WriteLine($"Team lost {awayWins} games.");
+            Console.WriteLine($"Drawn games: {draws}");
+        }
+    }
+}
