9ba653a6ce67615f17d7beede431d2fc6c63dc87
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-29 19:44:53 +0100

    Add files via upload

---
+++ Programming Basics Exams/PB-9and10March/Skeleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Skeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int controllMinutes = int.Parse(Console.ReadLine());
+            int controllSec = int.Parse(Console.ReadLine());
+            double lengthInM = double.Parse(Console.ReadLine());
+            double secToCover100M = double.Parse(Console.ReadLine());
+
+            double totalTimeControll = controllMinutes * 60 + controllSec;
+            double slowDownAmount = lengthInM / 120;
+            double totalSlowDown = slowDownAmount * 2.5;
+            double totalTime = (lengthInM / 100) * secToCover100M - totalSlowDown;
+
+           
+
+            if (totalTime <= totalTimeControll)
+            {
+                Console.WriteLine("Marin Bangiev won an Olympic quota!");
+                Console.WriteLine($"His time is {totalTime:f3}.");
+            }
+            else
+            {
+                double difference = Math.Abs(totalTimeControll - totalTime);
+                Console.WriteLine($"No, Marin failed! He was {difference:f3} second slower.");
+            }
+
+
+        }
+    }
+}
