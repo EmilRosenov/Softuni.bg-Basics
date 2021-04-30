a412b49e2eeef9e311e40dccb80b6b2193fc907c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-04-30 17:18:35 +0100

    Add files via upload

---
+++ Programming Basics Exams/PB-9and10March/TennisEqupment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TennisEqupment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceOfTennisRocket = double.Parse(Console.ReadLine());
+            int numberTennisRockets = int.Parse(Console.ReadLine());
+            int numberPairShoes = int.Parse(Console.ReadLine());
+
+            double pricePairShoes = priceOfTennisRocket / 6;
+            double totalCostShoes = numberPairShoes * pricePairShoes;
+            double totalCostTennisRocket = priceOfTennisRocket * numberTennisRockets;
+
+            double costEquipment = (totalCostTennisRocket + totalCostShoes) * 0.2;
+            double total = totalCostShoes + totalCostTennisRocket + costEquipment;
+
+            double priceDjokovic = total * 0.125;
+            double priceSponsors = total * 0.875;
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(priceDjokovic)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(priceSponsors)}");
+        }
+    }
+}
