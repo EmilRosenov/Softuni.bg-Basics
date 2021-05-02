aa68144e73f35f2a9662ffbdd31e0a7e4e36c3e0
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-02 16:15:17 +0100

    Add files via upload

---
+++ Programming Basics Exams/PB-9and10March/Gymnastics/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Gymnastics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string item = Console.ReadLine();
+            double level = 0;
+            double performance = 0;
+            double total = 0;
+
+            if (country == "Russia")
+            {
+                if (item == "ribbon")
+                {
+                    level = 9.100;
+                    performance = 9.400;
+                    total = level + performance;
+                }
+                else if (item == "hoop")
+                {
+                    level = 9.300;
+                    performance = 9.800;
+                    total = level + performance;
+                }
+                else if (item == "rope")
+                {
+                    level = 9.600;
+                    performance = 9.000;
+                    total = level + performance;
+                }
+            }
+            else if (country == "Bulgaria")
+            {
+                if (item == "ribbon")
+                {
+                    level = 9.600;
+                    performance = 9.400;
+                    total = level + performance;
+                }
+                else if (item == "hoop")
+                {
+                    level = 9.550;
+                    performance = 9.750;
+                    total = level + performance;
+                }
+                else if (item == "rope")
+                {
+                    level = 9.500;
+                    performance = 9.400;
+                    total = level + performance;
+                }
+            }
+            else if (country == "Italy")
+            {
+                if (item == "ribbon")
+                {
+                    level = 9.200;
+                    performance = 9.500;
+                    total = level + performance;
+                }
+                else if (item == "hoop")
+                {
+                    level = 9.450;
+                    performance = 9.350;
+                    total = level + performance;
+                }
+                else if (item == "rope")
+                {
+                    level = 9.700;
+                    performance = 9.150;
+                    total = level + performance;
+                }
+            }
+
+            Console.WriteLine($"The team of {country} get {total:f3} on {item}.");
+            double pointsNeeded = 20 - total;
+            double percentage = pointsNeeded / 20 * 100;
+            Console.WriteLine($"{percentage:f2}%");
+        }
+    }
+}
