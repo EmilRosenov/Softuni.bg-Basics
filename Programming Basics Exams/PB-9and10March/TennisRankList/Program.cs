3df11a260d1536a4480bee1773b892611f3cf134
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-07 03:32:29 +0100

    Add files via upload

---
+++ Programming Basics Exams/PB-9and10March/TennisRankList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TennisRankList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numberTournamentsParticipating = double.Parse(Console.ReadLine());
+            double startingPoints = double.Parse(Console.ReadLine());
+            double pointsGained = 0;
+            double winning = 0;
+
+            for (int i = 0; i < numberTournamentsParticipating; i++)
+            {
+                string stageReached = Console.ReadLine();
+
+                if (stageReached=="W")
+                {
+                    pointsGained += 2000;
+                    winning++;
+                }
+                else if (stageReached == "F")
+                {
+                    pointsGained += 1200;
+                }
+                else if (stageReached == "SF")
+                {
+                    pointsGained += 720;
+                }
+                 
+            }
+            double totalPoints = pointsGained + startingPoints;
+            
+            //double pointsPerYear = Math.Abs(startingPoints - pointsGained);
+            double averagePoints = pointsGained / numberTournamentsParticipating;
+            //double averagePoints = pointsPerYear / numberTournamentsParticipating;
+            double percentageWinning = winning / numberTournamentsParticipating * 100;
+
+            Console.WriteLine($"Final points: {totalPoints}");
+            Console.WriteLine($"Average points: {Math.Floor(averagePoints)}");
+            Console.WriteLine($"{percentageWinning:f2}%");
+        }
+    }
+}
