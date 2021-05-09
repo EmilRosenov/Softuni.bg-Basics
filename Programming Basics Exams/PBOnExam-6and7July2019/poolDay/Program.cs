8c544737613d034d31d893001c0a2eb967fb2a52
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-09 18:54:39 +0100

    Add files via upload

---
+++ Programming Basics Exams/PBOnExam-6and7July2019/poolDay/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace poolDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double people = double.Parse(Console.ReadLine());
+            double entranceFee = double.Parse(Console.ReadLine());
+            double priceSunbed = double.Parse(Console.ReadLine());
+            double priceUmbrella = double.Parse(Console.ReadLine());
+
+            double total = 0;
+
+            double totalEntryFee = people * entranceFee;
+            double numberUmbrellas = Math.Ceiling(people / 2);
+            double totalUmbrella = priceUmbrella * numberUmbrellas;
+            double numberSunbed = Math.Ceiling(people * 0.75);
+            double totalSunbed = numberSunbed * priceSunbed;
+
+            double totalCosts = totalEntryFee + totalUmbrella + totalSunbed;
+            Console.WriteLine($"{totalCosts:f2} lv.");
+        }
+    }
+}
