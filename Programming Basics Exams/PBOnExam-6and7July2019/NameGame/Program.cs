97d258734360a8e21d79e345fabe453a6d7e66c7
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-11 21:21:10 +0100

    Add files via upload

---
+++ Programming Basics Exams/PBOnExam-6and7July2019/NameGame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NameGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxPoints = 0;
+            string bestPlayer = "";
+            string name = Console.ReadLine();
+
+            while (name != "Stop")
+            {
+                int points = 0;
+
+                
+                    for (int i = 0; i < name.Length; i++)
+                    {
+                        int numberWritten = int.Parse(Console.ReadLine());
+
+                        if (numberWritten == name[i])
+                        {
+                            points += 10;
+                        }
+                        else
+                        {
+                            points += 2;
+                        }
+
+                        if (points >= maxPoints)
+                        {
+                            maxPoints = points;
+                            bestPlayer = name;
+                        }
+                    }
+                name = Console.ReadLine();
+            }
+            if (name == "Stop")
+            {
+                Console.WriteLine($"The winner is {bestPlayer} with {maxPoints} points!");
+            }
+        }
+    }
+}
