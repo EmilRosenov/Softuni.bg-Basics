1b9ac8adab884c47a3d997383b657ac566809dde
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-11 21:30:44 +0100

    Add files via upload

---
+++ Programming Basics Exams/PBOnExam-6and7July2019/footballTournament/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace footballTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string footballClub = Console.ReadLine();
+            double numberGamesPlayed = double.Parse(Console.ReadLine());
+
+            double numberWins=0;
+            double numberDraws=0;
+            double numberLosses=0;
+
+            double points = 0;
+
+            if (numberGamesPlayed == 0)
+            {
+                Console.WriteLine($"{footballClub} hasn't played any games during this season.");
+            }
+
+            else
+            {
+                for (int i = 1; i <= numberGamesPlayed; i++)
+                {
+                    char result = char.Parse(Console.ReadLine());
+
+                    if (result == 'W')
+                    {
+                        numberWins++;
+                        points += 3;
+                    }
+                    else if (result == 'D')
+                    {
+                        numberDraws++;
+                        points += 1;
+                    }
+                    else if (result == 'L')
+                    {
+                        numberLosses++;
+                    }
+                }
+
+                Console.WriteLine($"{footballClub} has won {points} points during this season.");
+                Console.WriteLine("Total stats:");
+                Console.WriteLine($"## W: {numberWins}");
+                Console.WriteLine($"## D: {numberDraws}");
+                Console.WriteLine($"## L: {numberLosses}");
+
+                double winRate = numberWins / numberGamesPlayed * 100;
+
+                Console.WriteLine($"Win rate: {winRate:f2}%");
+            }
+        }
+    }
+}
