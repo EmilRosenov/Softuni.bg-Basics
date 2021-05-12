a2f9c51c5c6bc2641e45156039206aa10afedaa5
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-12 17:14:14 +0100

    Add files via upload

---
+++ Programming Basics Exams/ProgramBasicsOnlineExam29 March/SchoolSupplies/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SchoolSupplies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberPenPacks = double.Parse(Console.ReadLine());
+            double numberMarkerPacks = double.Parse(Console.ReadLine());
+            double cleningBoardChemical = double.Parse(Console.ReadLine());
+            double discountPercentage = double.Parse(Console.ReadLine());
+
+            double penPacksPrice = 5.80;
+            double markersPacksPrice = 7.20;
+            double cleningBoardChemicalPrice = 1.20;
+
+            double price = (numberPenPacks * penPacksPrice) +
+                           (numberMarkerPacks* markersPacksPrice) + 
+                           (cleningBoardChemical* cleningBoardChemicalPrice);
+
+            double disount = price * discountPercentage / 100;
+            double total = price - disount;
+            Console.WriteLine($"{total:f3}");
+
+        }
+    }
+}
