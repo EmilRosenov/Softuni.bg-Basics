494e8ec6c3fe082c27ea96ba03679a73ac3950a0
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-12 17:26:40 +0100

    Add files via upload

---
+++ Programming Basics Exams/ProgramBasicsOnlineExam29 March/careForPuppy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace careForPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodBoughtKG = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            int foodLeftGrams = foodBoughtKG * 1000;
+
+            while (command != "Adopted")
+            {
+        
+                int gramsEatenPerMeal = int.Parse(command);
+
+                foodLeftGrams -= gramsEatenPerMeal;
+
+
+                command = Console.ReadLine();
+            }
+            if (foodLeftGrams >= 0)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodLeftGrams} grams.");
+            }
+            else
+            {
+                int difference = 0 - foodLeftGrams;
+                Console.WriteLine($"Food is not enough. You need {Math.Abs(difference)} grams more.");
+            }
+        }
+    }
+}
