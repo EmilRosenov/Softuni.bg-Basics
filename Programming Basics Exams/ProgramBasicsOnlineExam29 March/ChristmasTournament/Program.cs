8d99ab0619f0a9052b076bc365b3333608b51f4f
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-15 13:15:14 +0100

    Add files via upload

---
+++ Programming Basics Exams/ProgramBasicsOnlineExam29 March/ChristmasTournament/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ChristmasTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberDays = int.Parse(Console.ReadLine());
+
+
+           
+            double totalSum = 0;
+            
+            
+            int winningDays = 0;
+            int losingDays = 0;
+
+            for (int day = 1; day <= numberDays; day++)
+            {
+                string game = Console.ReadLine();
+                double sumPerDay = 0;
+                int numberWinsPerDay = 0;
+                int numberLosesPerDay = 0;
+
+                while (game !="Finish")
+                {
+                    
+                    string result = Console.ReadLine();
+
+                    
+
+                    if (result=="win")
+                    {
+                        sumPerDay += 20;
+                        numberWinsPerDay++;
+                    }
+                    else if (result == "lose")
+                    {
+                        numberLosesPerDay++;
+                    }
+
+
+                    game = Console.ReadLine();
+                }
+
+                if (numberWinsPerDay > numberLosesPerDay)
+                {
+                    sumPerDay = sumPerDay * 1.10;
+                    totalSum += sumPerDay;
+                    winningDays++;
+
+                }
+                else
+                {
+                    losingDays++;
+                    totalSum += sumPerDay;
+                }
+
+            }
+
+            if (winningDays > losingDays)
+            {
+                totalSum = totalSum * 1.20 ;
+                Console.WriteLine($"You won the tournament! Total raised money: {totalSum:f2}");
+                
+            }
+            else 
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {totalSum:f2}");
+                
+            }
+
+        }
+    }
+}
