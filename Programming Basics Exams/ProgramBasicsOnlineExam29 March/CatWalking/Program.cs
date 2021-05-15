615f37b79dff6474930232215886e1ae992bf9dc
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-15 13:15:58 +0100

    Add files via upload

---
+++ Programming Basics Exams/ProgramBasicsOnlineExam29 March/CatWalking/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CatWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double minutesPerDay = double.Parse(Console.ReadLine());
+            double numberWalksPerDay = double.Parse(Console.ReadLine());
+            double caloriesRecieved = double.Parse(Console.ReadLine());
+            double caloriesPerMinute = 5;
+
+            double totalMinutes = minutesPerDay * numberWalksPerDay;
+
+            double caloriesBurnt = totalMinutes * caloriesPerMinute;
+
+            if (caloriesRecieved * 0.5 <= caloriesBurnt)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {caloriesBurnt}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {caloriesBurnt}.");
+            }
+        }
+    }
+}
