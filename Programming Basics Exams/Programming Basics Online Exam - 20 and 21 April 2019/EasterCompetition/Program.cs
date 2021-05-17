0bdbeca563488bd9e2075d31a1269946e3b308e8
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-17 18:24:21 +0100

    Add files via upload

---
+++ Programming Basics Exams/Programming Basics Online Exam - 20 and 21 April 2019/EasterCompetition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasterCompetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
