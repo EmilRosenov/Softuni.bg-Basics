a91ce6f7346defe8e0ca0f1e17460dc8b1428e79
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-17 18:22:56 +0100

    Add files via upload

---
+++ Programming Basics Exams/Programming Basics Online Exam - 20 and 21 April 2019/EasterBakery/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EasterBakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyNeeded = 0;
+
+            double flourPrice = double.Parse(Console.ReadLine());
+            double kgsFlour = double.Parse(Console.ReadLine());
+            double kgsSugar = double.Parse(Console.ReadLine());
+            int numberDozensEggs = int.Parse(Console.ReadLine());
+            int numberPacksYeast = int.Parse(Console.ReadLine());
+
+            double totalMoneyForFlour = flourPrice * kgsFlour;
+            double sugarPrice = flourPrice * 0.75;
+            double totalMoneyForSugar = sugarPrice * kgsSugar;
+            double totalMoneyForEggs = flourPrice *1.10 * numberDozensEggs;
+            double totalMoneyForYeast = numberPacksYeast * (sugarPrice*0.2);
+
+            moneyNeeded = totalMoneyForFlour + totalMoneyForSugar + totalMoneyForEggs + totalMoneyForYeast;
+
+            Console.WriteLine($"{moneyNeeded:f2}");
+        }
+    }
+}
