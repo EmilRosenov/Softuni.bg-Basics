07607961149d4d6422e80b68b93824826a7a46b3
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-18 14:32:20 +0100

    Add files via upload

---
+++ Programming Basics Exams/Programming Basics Online Exam - 20 and 21 April 2019/EasterTrip/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace EasterTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            string dates = Console.ReadLine();
+            int numberNights = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (destination=="France")
+            {
+                if (dates== "21-23")
+                {
+                    price = numberNights * 30;
+                }
+                else if (dates=="24-27")
+                {
+                    price = numberNights * 35;
+                }
+                else if(dates=="28-31")
+                {
+                    price = numberNights * 40;
+                }
+            }
+            else if (destination == "Italy")
+            {
+                if (dates == "21-23")
+                {
+                    price = numberNights * 28;
+                }
+                else if (dates == "24-27")
+                {
+                    price = numberNights * 32;
+                }
+                else if (dates == "28-31")
+                {
+                    price = numberNights * 39;
+                }
+            }
+            else if (destination == "Germany")
+            {
+                if (dates == "21-23")
+                {
+                    price = numberNights * 32;
+                }
+                else if (dates == "24-27")
+                {
+                    price = numberNights * 37;
+                }
+                else if (dates == "28-31")
+                {
+                    price = numberNights * 43;
+                }
+            }
+
+            Console.WriteLine($"Easter trip to {destination} : {price:f2} leva.");
+        }
+    }
+}
