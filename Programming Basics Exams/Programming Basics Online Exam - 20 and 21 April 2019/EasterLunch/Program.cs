0a038001d7c50f0a06ddd5b571da34b5baed1f54
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-18 14:30:15 +0100

    Add files via upload

---
+++ Programming Basics Exams/Programming Basics Online Exam - 20 and 21 April 2019/EasterLunch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EasterLunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalCost = 0;
+            double cosunac = 3.20;
+            double eggs12 = 4.35;
+            double cookiesPricePerKG = 5.40;
+            double eggPaintForAnEgg = 0.15;
+
+            int numberCosunacs = int.Parse(Console.ReadLine());
+            int dozensOfEggs = int.Parse(Console.ReadLine());
+            int kgsCookies = int.Parse(Console.ReadLine());
+
+            double moneyForCosunacs = numberCosunacs * cosunac;
+            double moneyForEggs = dozensOfEggs * eggs12;
+            double moneyForCookies = cookiesPricePerKG * kgsCookies;
+            double moneyForEggPaint = dozensOfEggs * 12 * eggPaintForAnEgg;
+
+            totalCost = moneyForCosunacs + moneyForEggs + moneyForCookies + moneyForEggPaint;
+
+            Console.WriteLine($"{totalCost:f2}");
+        }
+    }
+}
