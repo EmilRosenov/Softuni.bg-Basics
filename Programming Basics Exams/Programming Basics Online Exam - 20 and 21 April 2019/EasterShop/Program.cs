0c5643b5b15acdb2c14be78516359b63f3489d0a
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-18 14:31:45 +0100

    Add files via upload

---
+++ Programming Basics Exams/Programming Basics Online Exam - 20 and 21 April 2019/EasterShop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EasterShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfEgssBeginning = int.Parse(Console.ReadLine());
+
+            int currentNumber = amountOfEgssBeginning;
+            int numberEggsSold = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "Close")
+            {
+                if (command == "Buy")
+                {
+                    int amountOfEggsToBeBought = int.Parse(Console.ReadLine());
+
+                    
+                    numberEggsSold += amountOfEggsToBeBought;
+
+                    if (amountOfEggsToBeBought > currentNumber)
+                    {
+                        Console.WriteLine("Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {currentNumber}.");
+                        break;
+                    }
+                    currentNumber -= amountOfEggsToBeBought;
+
+                }
+                else if (command == "Fill")
+                {
+                    int amountOfEggsToBeAdded = int.Parse(Console.ReadLine());
+
+                    currentNumber += amountOfEggsToBeAdded;
+                }
+
+
+                
+
+                command = Console.ReadLine();
+            }
+            if (command == "Close")
+            {
+                Console.WriteLine("Store is closed!");
+                Console.WriteLine($"{numberEggsSold} eggs sold.");
+
+            }
+        }
+    }
+}
